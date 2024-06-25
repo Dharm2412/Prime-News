75f33f58942854e7c057555ce814e609180bf63f
dharm <dpp6402@gmail.com>
2024-06-25 20:35:55 +0530

    update

--- src/Components/Contect.js
+++ src/Components/Contect.js
@@ -5,14 +5,14 @@ const ContactUs = () => {
     <div className="zigbee">
       <div className="containerz">
         <h2>Contact Us </h2>
-        <form action="https://fabform.io/f/{insert-form-id-here}" method="post">
+        <form method="POST" action="https://formspree.io/f/xvgppaow">
           <div className="form-group">
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required="" />
+            <input type="text" id="name" name="name" required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required="" />
+            <input type="email" id="email" name="email" required />
           </div>
           <div className="form-group">
             <label htmlFor="message">Message:</label>
@@ -26,15 +26,6 @@ const ContactUs = () => {
           <div className="form-group">
             <button type="submit">Submit</button>
           </div>
-          <p className="my-4">
-            If you prefer to contact us by email.{" "}
-            <a
-              className="text-blue-500 hover:underline"
-              href="https://veilmail.io/e/FkKh7o"
-            >
-              https://veilmail.io/e/FkKh7o
-            </a>
-          </p>
         </form>
       </div>
     </div>
