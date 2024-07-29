8b0e60a4711a3a2ff0e05ec0e2ce37a25469d635
Dharm Patel <142787486+Dharm2412@users.noreply.github.com>
2024-07-29 21:29:34 +0530

    Update Contect.js

--- src/Components/Contect.js
+++ src/Components/Contect.js
@@ -1,35 +1,35 @@
-// import React from "react";
+import React from "react";
 
-// const ContactUs = () => {
-//   return (
-//     <div className="zigbee">
-//       <div className="containerz">
-//         <h2>Contact Us </h2>
-//         <form method="POST" action="https://formspree.io/f/xvgppaow">
-//           <div className="form-group">
-//             <label htmlFor="name">Name:</label>
-//             <input type="text" id="name" name="name" required />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="email">Email:</label>
-//             <input type="email" id="email" name="email" required />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="message">Message:</label>
-//             <textarea
-//               id="message"
-//               name="message"
-//               required=""
-//               defaultValue={""}
-//             />
-//           </div>
-//           <div className="form-group">
-//             <button type="submit">Submit</button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
+const ContactUs = () => {
+  return (
+    <div className="zigbee">
+      <div className="containerz">
+        <h2>Contact Us </h2>
+        <form method="POST" action="https://formspree.io/f/xvgppaow">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              required=""
+              defaultValue={""}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-// export default ContactUs;
+export default ContactUs;
